733a88d775495ec9bd683aaa125fccd0e7707ec2
Avshalom-Z <avshalomz.ts@gmail.com>
2025-06-25 22:48:38 +0300

    Add fastfetch and alacritty configuration files with symbolic links in after-clean-install.sh

--- Windows-managers/QtileConfigs/after-clean-install.sh
+++ Windows-managers/QtileConfigs/after-clean-install.sh
@@ -184,15 +184,30 @@ else
     echo "fastfetch config file already exists. Skipping symbolic link creation."
 fi
 
+if [ ! -f "$HOME/.config/fastfetch/config-v2.jsonc" ]; then
+    echo "Creating symbolic link for fastfetch config file..."
+    ln -s "$HOME/Linux-Files/Windows-managers/QtileConfigs/.config/fastfetch/config-v2.jsonc" "$HOME/.config/fastfetch/config-v2.jsonc"
+else
+    echo "fastfetch config file already exists. Skipping symbolic link creation."
+fi
+
+if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
+    echo "Creating symbolic link for fastfetch config file..."
+    ln -s "$HOME/Linux-Files/Windows-managers/QtileConfigs/.config/fastfetch/config.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+else
+    echo "fastfetch config file already exists. Skipping symbolic link creation."
+fi
+
 # Create a symbolic link for alacritty config file
 if [ ! -d "$HOME/.config/alacritty" ]; then
     echo "Creating alacritty config directory..."
     mkdir -p "$HOME/.config/alacritty"
 fi
 
-if [ ! -f "$HOME/.config/alacritty/alacritty.yml" ]; then
+if [ ! -f "$HOME/.config/alacritty/alacritty.toml" ]; then
     echo "Creating symbolic link for alacritty config file..."
-    ln -s "$HOME/Linux-Files/Windows-managers/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+    ln -s "$HOME/Linux-Files/Windows-managers/QtileConfigs/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+    
 else
     echo "Alacritty config file already exists. Skipping symbolic link creation."
 fi
