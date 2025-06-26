e95995d78d12fff91ce1df1a509cb3c25b6f169b
Avshalom-Z <avshalomz.ts@gmail.com>
2025-06-26 17:34:26 +0300

    Adjust layout border width and margin in config.py; change bar position to top

--- Windows-managers/QtileConfigs/.config/qtile/config.py
+++ Windows-managers/QtileConfigs/.config/qtile/config.py
@@ -123,7 +123,7 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2, margin=8),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -147,7 +147,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
