b847df92b9cc59cc2214bfcb6d52385fe81c6a00
Avshalom-Ts <avshalomz.ts@gmail.com>
2025-06-28 21:33:44 +0000

    Add LightDM configuration files and update copy script

---
+++ Windows-managers/QtileConfigs/scripts/lightdm-conf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo
+tput setaf 2
+echo "#################################################################"
+echo "######################### Copy LightDM Conf #####################"
+echo "#################################################################"
+tput sgr0
+echo
+
+
+# Copy the lightdm configuration file to the appropriate directory
+sudo cp /home/azarviv/Linux-Files/Windows-managers/QtileConfigs/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
+# Copy the lightdm-webkit2-greeter configuration file to the appropriate directory
+sudo cp /home/azarviv/Linux-Files/Windows-managers/QtileConfigs/etc/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
